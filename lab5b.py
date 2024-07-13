8c1d38529ed95c36c49c4deb0041e409f800cf5c
Bishwash BC <bb-c1@Bishwash>
2024-07-12 20:55:14 -0400

    added lab5b.py

---
+++ lab5b.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Author ID: bb-c1
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    with open(file_name, 'r') as file:
+        return file.read()
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name,
+    # return its entire contents as a list of lines without new-line characters
+    with open(file_name, 'r') as file:
+        return [line.strip() for line in file]
+
+def read_file_string(file_name):
+    with open(file_name, 'r') as file:
+        return file.read()
+
+def read_file_list(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        # Remove newline characters from each line
+        lines = [line.rstrip('\n') for line in lines]
+        return lines
+
+def append_file_string(file_name, string_of_lines):
+    with open(file_name, 'a') as f:
+        f.write(string_of_lines)
+
+def write_file_list(file_name, list_of_lines):
+    with open(file_name, 'w') as f:
+        for line in list_of_lines:
+            f.write(line + '\n')
+
+def copy_file_add_line_numbers(file_name_read, file_name_write):
+    with open(file_name_read, 'r') as fr:
+        lines = fr.readlines()
+    with open(file_name_write, 'w') as fw:
+        for idx, line in enumerate(lines, start=1):
+            fw.write(f"{idx}:{line}")
+
+if __name__ == '__main__':
+    file1 = 'seneca1.txt'
+    file2 = 'seneca2.txt'
+    file3 = 'seneca3.txt'
+    string1 = 'First Line\nSecond Line\nThird Line\n'
+    list1 = ['Line 1', 'Line 2', 'Line 3']
+
+    append_file_string(file1, string1)
+    print(read_file_string(file1))
+
+    write_file_list(file2, list1)
+    print(read_file_string(file2))
+
+    copy_file_add_line_numbers(file2, file3)
+    print(read_file_string(file3))
